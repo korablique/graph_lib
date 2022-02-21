db01675341b716588168400a0e234105b55acb1f
korablique <yulia-11a@yandex.ru>
2022-02-21 16:26:38 +0300

    add test fixture

--- Google_tests/graph_methods_test.cpp
+++ Google_tests/graph_methods_test.cpp
@@ -1,64 +1,72 @@
 #include "gtest/gtest.h"
 #include "../GraphsMethods.h"
 
-TEST(GetDegreeListTest, WorksForSimpleGraphs) {
-    size_t matrix_size = 4;
-    std::vector<std::vector<bool>> adjacency_matrix(matrix_size);
-    for (int i = 0; i < matrix_size; i++) {
-        adjacency_matrix[i] = std::vector<bool>(matrix_size, false);
+class GraphMethodsTest : public ::testing::Test {
+protected:
+    // for disconnected simple graph
+    std::vector<std::vector<bool>> adj_matrix_disconnected_simple;
+    // for connected simple graph
+    std::vector<std::vector<bool>> adj_matrix_connected_simple;
+    // for connected weighted simple graph
+    std::vector<std::vector<double>> adj_matrix_connected_weighted_simple;
+
+    void SetUp() override {
+        // for disconnected simple graph
+        size_t matrix_size = 4;
+        for (int i = 0; i < matrix_size; i++) {
+            adj_matrix_disconnected_simple.push_back(std::vector<bool>(matrix_size, false));
+        }
+        adj_matrix_disconnected_simple[0][1] = true;
+        adj_matrix_disconnected_simple[1][0] = true;
+        adj_matrix_disconnected_simple[0][2] = true;
+        adj_matrix_disconnected_simple[2][0] = true;
+
+        // for connected simple graph
+        adj_matrix_connected_simple = adj_matrix_disconnected_simple;
+        adj_matrix_connected_simple[2][3] = true;
+        adj_matrix_connected_simple[3][2] = true;
+
+        // for connected weighted simple graph
+        double inf = std::numeric_limits<double>::max();
+        for (int i = 0; i < matrix_size; i++) {
+            adj_matrix_connected_weighted_simple.push_back(std::vector<double>(matrix_size, inf));
+        }
+        adj_matrix_connected_weighted_simple[0][0] = 0;
+        adj_matrix_connected_weighted_simple[1][1] = 0;
+        adj_matrix_connected_weighted_simple[2][2] = 0;
+        adj_matrix_connected_weighted_simple[3][3] = 4;
+        adj_matrix_connected_weighted_simple[0][1] = 4;
+        adj_matrix_connected_weighted_simple[0][2] = 3;
+        adj_matrix_connected_weighted_simple[1][0] = 4;
+        adj_matrix_connected_weighted_simple[2][0] = 3;
     }
-    adjacency_matrix[0][1] = true;
-    adjacency_matrix[0][2] = true;
-    adjacency_matrix[1][0] = true;
-    adjacency_matrix[2][0] = true;
-    auto degree_list = getDegreeList(adjacency_matrix);
+};
+
+
+TEST_F(GraphMethodsTest, GetDegreeListWorksForSimpleGraphs) {
+    auto degree_list = getDegreeList(adj_matrix_disconnected_simple);
     std::vector<size_t> expected({2, 1, 1, 0});
     EXPECT_EQ(degree_list, expected);
 }
 
-TEST(GetDegreeListTest, WorksForWeightedSimpleGraphs) {
-    size_t matrix_size = 4;
-    double inf = std::numeric_limits<double>::max();
-    std::vector<std::vector<double>> adjacency_matrix(matrix_size);
-    for (int i = 0; i < matrix_size; i++) {
-        adjacency_matrix[i] = std::vector<double>(matrix_size, inf);
-    }
-    adjacency_matrix[0][0] = 0;
-    adjacency_matrix[1][1] = 0;
-    adjacency_matrix[2][2] = 0;
-    adjacency_matrix[3][3] = 4;
-    adjacency_matrix[0][1] = 4;
-    adjacency_matrix[0][2] = 3;
-    adjacency_matrix[1][0] = 4;
-    adjacency_matrix[2][0] = 3;
-
-    auto degree_list = getDegreeList(adjacency_matrix);
+TEST_F(GraphMethodsTest, GetDegreeListWorksForWeightedSimpleGraphs) {
+    auto degree_list = getDegreeList(adj_matrix_connected_weighted_simple);
     std::vector<size_t> expected({2, 1, 1, 0});
     EXPECT_EQ(degree_list, expected);
 }
 
-TEST(GetAdjacencyListTest, WorksForSimpleGraphs) {
-    // create adjacency matrix
-    size_t matrix_size = 4;
-    std::vector<std::vector<bool>> adjacency_matrix(matrix_size);
-    for (int i = 0; i < matrix_size; i++) {
-        adjacency_matrix[i] = std::vector<bool>(matrix_size, false);
-    }
-    adjacency_matrix[0][1] = true;
-    adjacency_matrix[0][2] = true;
-    adjacency_matrix[1][0] = true;
-    adjacency_matrix[2][0] = true;
-
+TEST_F(GraphMethodsTest, GetAdjacencyListWorksForSimpleGraphs) {
     // TODO for the first time there is int instead of Node
-    std::vector<std::vector<int>> expected(matrix_size);
-    expected[0] = std::vector<int>({1, 2});
-    expected[1] = std::vector<int>({0});
-    expected[2] = std::vector<int>({0});
-    expected[3] = std::vector<int>();
+    std::vector<std::vector<int>> expected;
+    expected.push_back(std::vector<int>({1, 2}));
+    expected.push_back(std::vector<int>({0}));
+    expected.push_back(std::vector<int>({0}));
+    expected.push_back(std::vector<int>());
 
-    auto adj_list = getAdjacencyList(adjacency_matrix);
+    auto adj_list = getAdjacencyList(adj_matrix_disconnected_simple);
 
-    for (int i = 0; i < matrix_size; i++) {
+    EXPECT_EQ(adj_list.size(), expected.size());
+    for (int i = 0; i < expected.size(); i++) {
         EXPECT_EQ(adj_list[i], expected[i]);
     }
 }
@@ -78,40 +86,20 @@ TEST(GetAdjacencyListTest, WorksForDirectedGraphs) {
     expected[0] = std::vector<int>({1, 2});
 
     auto adj_list = getAdjacencyList(adjacency_matrix);
-    for (int i = 0; i < matrix_size; i++) {
+
+    EXPECT_EQ(adj_list.size(), expected.size());
+    for (int i = 0; i < expected.size(); i++) {
         EXPECT_EQ(adj_list[i], expected[i]);
     }
 }
 
-TEST(IsConnectedTest, WorksForDisconnectedGraph) {
-    size_t matrix_size = 4;
-    std::vector<std::vector<bool>> adjacency_matrix(matrix_size);
-    for (int i = 0; i < matrix_size; i++) {
-        adjacency_matrix[i] = std::vector<bool>(matrix_size, false);
-    }
-    adjacency_matrix[0][1] = true;
-    adjacency_matrix[0][2] = true;
-    adjacency_matrix[1][0] = true;
-    adjacency_matrix[2][0] = true;
-
-    bool is_connected = isConnected(adjacency_matrix);
+TEST_F(GraphMethodsTest, IsConnectedWorksForDisconnectedGraph) {
+    bool is_connected = isConnected(adj_matrix_disconnected_simple);
     EXPECT_FALSE(is_connected);
 }
 
-TEST(IsConnectedTest, WorksForConnectedGraph) {
-    size_t matrix_size = 4;
-    std::vector<std::vector<bool>> adjacency_matrix(matrix_size);
-    for (int i = 0; i < matrix_size; i++) {
-        adjacency_matrix[i] = std::vector<bool>(matrix_size, false);
-    }
-    adjacency_matrix[0][1] = true;
-    adjacency_matrix[0][2] = true;
-    adjacency_matrix[1][0] = true;
-    adjacency_matrix[2][0] = true;
-    adjacency_matrix[2][3] = true;
-    adjacency_matrix[3][2] = true;
-
-    bool is_connected = isConnected(adjacency_matrix);
+TEST_F(GraphMethodsTest, IsConnectedWorksForConnectedSimpleGraph) {
+    bool is_connected = isConnected(adj_matrix_connected_simple);
     EXPECT_TRUE(is_connected);
 }
 
